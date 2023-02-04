cd707642f599296aad99eee2fa3050c7e6305dcd
huongnguyenduc <duchuong007@gmail.com>
2023-02-04 19:09:30 +0700

    feat: update get nodes data

--- src/lib/hooks/store/node.ts
+++ src/lib/hooks/store/node.ts
@@ -1,19 +1,21 @@
-import { initialEdges, initialNodes } from "lib/dummy";
+import styles from "components/node/Flow.module.css";
 import {
   addEdge,
   applyEdgeChanges,
   applyNodeChanges,
   Connection,
   EdgeChange,
+  MarkerType,
   Node,
   NodeChange,
 } from "reactflow";
-import { BlockchainNode, NodeState } from "types";
+import { getNodes } from "services/nodes";
+import { BlockchainNode, NodeResponse, NodeState } from "types";
 import { create } from "zustand";
 
-const useNodeStore = create<NodeState>((set, get) => ({
-  nodes: initialNodes,
-  edges: initialEdges,
+const useNodeStore = create<NodeState>((set, get, store) => ({
+  nodes: [],
+  edges: [],
   selectedNode: null,
   onNodeSelect: (node: Node<BlockchainNode> | null) => {
     set({ selectedNode: node });
@@ -31,6 +33,115 @@ const useNodeStore = create<NodeState>((set, get) => ({
   onConnect: (connection: Connection) => {
     set({
       edges: addEdge(connection, get().edges),
+    });
+  },
+  loadNodes: async () => {
+    const nodeResponse: NodeResponse = await getNodes();
+    if (!nodeResponse || !nodeResponse.nodes) {
+      set({
+        nodes: [],
+        edges: [],
+      });
+      return;
+    }
+
+    const nodes = nodeResponse.nodes;
+
+    if (nodes.length !== get().nodes.length) {
+      set({
+        nodes: [],
+        edges: [],
+      });
+    } else {
+      const isSameNodes = nodes.every((node) => {
+        const existingNode = get().nodes.find((n) => n.id === node.account);
+        return (
+          existingNode &&
+          existingNode.data.ip === node.ip &&
+          existingNode.data.port === node.port &&
+          existingNode.data.is_bootstrap === node.is_bootstrap
+        );
+      });
+
+      if (isSameNodes) {
+        return;
+      }
+    }
+
+    // Retrieve bootstrap node
+    const bootstrapNode = nodes.find((node) => node.is_bootstrap);
+
+    if (!bootstrapNode) {
+      return;
+    }
+
+    // Map nodes to nodes
+    const nodesToAdd = nodes.map((node, index) => ({
+      id: node.account,
+      data: node,
+      position: {
+        y: Math.random() * 800,
+        x: (Math.random() + (index % 2) === 0 ? -index : index) * 400,
+      },
+      type: "customNode",
+      className: styles.customNode,
+    }));
+
+    // Map nodes to edges
+    const edgesToAdd = nodesToAdd
+      .filter((n) => !n.data.is_bootstrap)
+      .map((node) => ({
+        id: `${bootstrapNode.account}-${node.data.account}`,
+        source: bootstrapNode.account,
+        target: node.data.account,
+        // type: "smoothstep",
+        arrowHeadType: "arrowclosed",
+        animated: true,
+        markerEnd: {
+          type: MarkerType.ArrowClosed,
+        },
+      }));
+
+    set({
+      nodes: nodesToAdd,
+      edges: edgesToAdd,
+    });
+
+    // Get peer nodes for each node
+    const peers = await Promise.all(
+      nodes
+        .filter((node) => !node.is_bootstrap)
+        .map(async (node) => {
+          const peersResponse = await getNodes({
+            ip: node.ip,
+            port: node.port,
+          });
+          return peersResponse.nodes;
+        })
+    );
+
+    // Map peer nodes to edges
+    peers.forEach((peerNodes: BlockchainNode[], index) => {
+      const connectNode = nodes.filter((node) => !node.is_bootstrap)[index];
+      if (!connectNode) {
+        return;
+      }
+      const edgesToAdd = peerNodes
+        .filter((node) => node.account !== connectNode.account)
+        .map((node) => ({
+          id: `${connectNode.account}-${node.account}`,
+          source: connectNode.account,
+          target: node.account,
+          // type: "smoothstep",
+          arrowHeadType: "arrowclosed",
+          animated: true,
+          markerEnd: {
+            type: MarkerType.ArrowClosed,
+          },
+        }));
+      set({
+        edges: get().edges.concat(edgesToAdd),
+      });
     });
   },
 }));
