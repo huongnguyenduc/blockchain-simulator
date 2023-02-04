01a1b4d5cbd60f3b8cef83183f1968caf987e760
huongnguyenduc <duchuong007@gmail.com>
2023-02-04 19:06:21 +0700

    chore: add read env variable

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  publicRuntimeConfig: {
+    // API
+    // API_URL: process.env.API_URL,
+    BOOTSTRAP_IP: process.env.BOOTSTRAP_IP,
+    BOOTSTRAP_PORT: process.env.BOOTSTRAP_PORT,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
